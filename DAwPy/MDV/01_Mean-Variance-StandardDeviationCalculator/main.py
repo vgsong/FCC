127fbc1a8e948e2586bb755b892310dcbb9a183e
Victor Song <59714002+vgsong@users.noreply.github.com>
2022-01-09 18:36:08 -0800

    Add files via upload

---
+++ DAwPy/MDV/01_Mean-Variance-StandardDeviationCalculator/main.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def calculate(alista):
+    if not len(alista) == 9:
+        raise ValueError('List must contain nine numbers.')
+    else:
+        np_funcs = [
+            np.mean,
+            np.var,
+            np.std,
+            np.max,
+            np.min,
+            np.sum
+        ]
+
+        final = {
+            'mean': [],
+            'variance': [],
+            'standard deviation': [],
+            'max': [],
+            'min': [],
+            'sum': [],
+        }
+
+        np_arr = np.asarray(alista).reshape(3, 3)
+
+        result_ax0 = [f(np_arr, axis=0).tolist() for f in np_funcs]
+        result_ax1 = [f(np_arr, axis=1).tolist() for f in np_funcs]
+        result_flat = [f(np_arr) for f in np_funcs]
+
+        for k, ax0, ax1, flat in zip(final.keys(), result_ax0, result_ax1, result_flat):
+            final[k] = [ax0, ax1, flat]
+
+        return final
